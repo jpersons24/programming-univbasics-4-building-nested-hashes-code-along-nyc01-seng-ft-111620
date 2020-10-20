9b53add9330de58587c49f9c5a131d4e00209b3f
Jakob Persons <jakobep24@gmail.com>
2020-10-20 20:45:06 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -16,8 +16,8 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        first_friend = {:name => "Steven", :age => "30", :attitude => "confused"},
-        second_friend{:name => "Nurse", :age => "44", :attitude => "worried"}
+        {:name => "Steven", :age => "30", :attitude => "confused"},
+        {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
 }
